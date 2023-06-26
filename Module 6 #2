784a51c8fbd6b5e9fda01a9690086c03b981b529
whitneyrush <111330133+whitneyrush@users.noreply.github.com>
2023-06-25 23:48:24 -0400

    Update and rename from datetime import timedelta.py to Module 6 #2

---
+++ Module 6 #2
@@ -0,0 +1,3 @@
+from datetime import datetime, timedelta
+
+endtime = (datetime.now() - timedelta(seconds=60)).strftime('%H:%M:%S')
--- from datetime import timedelta.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime, timedelta
-
-endtime = (datetime.now() - timedelta(seconds=60)).strftime('%H:%M:%S')
