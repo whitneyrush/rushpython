551a8d6c64146ed2764a1dfefa828bfd47c696a4
whitneyrush <111330133+whitneyrush@users.noreply.github.com>
2023-06-25 23:32:41 -0400

    Add files via upload
    4. A little confusing because these are datetime objects, not sure how to integrate feet and inches

---
+++ d = timedelta(days=100, hours=10, minute.py
@@ -0,0 +1,3 @@
+from datetime import timedelta 
+obj = timedelta(days=100, hours=10, minutes=13)
+print(obj.days, obj.seconds, obj.microseconds) 
---
+++ import sys.py
@@ -0,0 +1,17 @@
+import sys
+from datetime import datetime
+from datetime import time
+from datetime import date
+def main():
+
+    dt = datetime.now()
+   #utc = datetime.utcnow()
+    time_string = dt.strftime("%X")
+    """https://strftime.org"""
+
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        if len(data) == 6:
+            _date, _time, store, item, cost, payment = data
+            print ("{dt}\t{time_string}\t{store}\t{item}\t{cost}\t{payment}")
+main()
