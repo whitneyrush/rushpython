994aa420f48a0e09b4462f0e0adcda6d282121eb
whitneyrush <111330133+whitneyrush@users.noreply.github.com>
2023-06-25 23:34:52 -0400

    Update d = timedelta(days=100, hours=10, minute.py

--- d = timedelta(days=100, hours=10, minute.py
+++ d = timedelta(days=100, hours=10, minute.py
@@ -1,3 +1,3 @@
-from datetime import timedelta 
-obj = timedelta(days=100, hours=10, minutes=13)
-print(obj.days, obj.seconds, obj.microseconds) 
+from datetime import timedelta
+d = timedelta(days=100, hours=10, minutes=13)
+print(d.days, d.seconds, d.microseconds)
